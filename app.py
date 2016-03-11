e22db2bf9b16777b4dc9b9049c4dd693ba586798
Mark Fontana <markfontana@gmail.com>
2016-03-11 10:59:15 -0500

    Update app.py

--- app.py
+++ app.py
@@ -18,26 +18,30 @@ def index():
 def post_ticker():
   if request.method == 'POST':
     stock = request.form['ticker']
-    #df = Quandl.get("WIKI/"+text.upper(),returns="pandas", authtoken="qCQkVD-2dfsdr6Sx4e2b")
-    #stock_close = np.array(df[df.index >= '2016-02-20']['Close']) 
-    #stock_dates = np.array(df[df.index >= '2016-02-20'].index, dtype=np.datetime64)
-    stock_close = np.random.random(50)
-    stock_dates = np.arange(50)
-    window_size = 30
-    window = np.ones(window_size)/float(window_size)
-    # create a new plot with a a datetime axis type
-    p = figure(width=800, height=350, x_axis_type="datetime")
-    # add renderers
-    p.line(stock_dates, stock_close, color='navy', legend='Close Price')
-    # customize
-    p.title = stock + " One-Month Average"
-    p.grid.grid_line_alpha=0
-    p.xaxis.axis_label = 'Date'
-    p.yaxis.axis_label = 'Price'
-    p.ygrid.band_fill_color="olive"
-    p.ygrid.band_fill_alpha = 0.1
-    script, div = components(p)
-    return render_template('graph.html', script=script, div=div)
+    return redirect(url_for('graph'))
+  
+@app.route('/graph', methods=['GET',POST'])
+def graph():
+  #df = Quandl.get("WIKI/"+text.upper(),returns="pandas", authtoken="qCQkVD-2dfsdr6Sx4e2b")
+  #stock_close = np.array(df[df.index >= '2016-02-20']['Close']) 
+  #stock_dates = np.array(df[df.index >= '2016-02-20'].index, dtype=np.datetime64)
+  stock_close = np.random.random(50)
+  stock_dates = np.arange(50)
+  window_size = 30
+  window = np.ones(window_size)/float(window_size)
+  # create a new plot with a a datetime axis type
+  p = figure(width=800, height=350, x_axis_type="datetime")
+  # add renderers
+  p.line(stock_dates, stock_close, color='navy', legend='Close Price')
+  # customize
+  p.title = stock + " One-Month Average"
+  p.grid.grid_line_alpha=0
+  p.xaxis.axis_label = 'Date'
+  p.yaxis.axis_label = 'Price'
+  p.ygrid.band_fill_color="olive"
+  p.ygrid.band_fill_alpha = 0.1
+  script, div = components(p)
+  return render_template('graph.html', script=script, div=div)
 
 if __name__ == '__main__':
   app.run(port=33507)
