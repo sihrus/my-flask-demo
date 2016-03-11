ca24cb35dece5366ecf0c6c1730b4be3eed21594
Mark Fontana <markfontana@gmail.com>
2016-03-11 10:30:41 -0500

    Update app.py

--- app.py
+++ app.py
@@ -2,6 +2,7 @@ from flask import Flask, render_template, request, redirect
 import Quandl
 import numpy as np
 from bokeh.plotting import figure, output_notebook, show
+from bokeh.embed import components
 
 app = Flask(__name__)
 
@@ -15,18 +16,13 @@ def index():
 
 @app.route('/', methods=['POST'])
 def post_ticker():
-    text = request.form['ticker']
-    get_ticker_graph(text.upper())
-
-def get_ticker_graph(stock):
-  df = Quandl.get("WIKI/"+stock,returns="pandas", authtoken="qCQkVD-2dfsdr6Sx4e2b")
+  text = request.form['ticker']
+  df = Quandl.get("WIKI/"+text.upper(),returns="pandas", authtoken="qCQkVD-2dfsdr6Sx4e2b")
   # prepare some data
   stock_close = np.array(df[df.index >= '2016-02-20']['Close']) #np.random.random(50)#
   stock_dates = np.array(df[df.index >= '2016-02-20'].index, dtype=np.datetime64) #np.arange(50) #
   window_size = 30
   window = np.ones(window_size)/float(window_size)
-  # output to static HTML file
-  output_file("stock_close.html", title="One-Month Closing Stock Prices")
   # create a new plot with a a datetime axis type
   p = figure(width=800, height=350, x_axis_type="datetime")
   # add renderers
@@ -38,8 +34,8 @@ def get_ticker_graph(stock):
   p.yaxis.axis_label = 'Price'
   p.ygrid.band_fill_color="olive"
   p.ygrid.band_fill_alpha = 0.1
-  # show the results
-  show(p)
+  script, div = components(p)
+  return render_template('graph.html', script=script, div=div)
 
 if __name__ == '__main__':
   app.run(port=33507)
